9cd9f87027bbe34d43a0a6352ff894831d579e0a
admin <admin@priyambodo.altostrat.com>
2025-02-01 05:59:14 +0000

    ARRAKIS deployed to https://arrakis-rzmyhdhywa-uc.a.run.app/

---
+++ script/sh05.tunnel.sh
@@ -0,0 +1,6 @@
+gcloud workstations start-tcp-tunnel \
+  --project=work-mylab-machinelearning \
+  --region=us-central1 \
+  --cluster=cluster-priyambodo-workstation \
+  --config=config-priyambodo-workstation \
+  vscode-priyambodo-workstation 22
---
+++ sh03.deploynow.sh
@@ -0,0 +1,1 @@
+script/sh03.deploynow.sh
